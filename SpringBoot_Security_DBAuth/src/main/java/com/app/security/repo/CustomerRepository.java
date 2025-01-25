7a78eea2d863232eab0a627f07c64dafbb40f543
Mohit Kumar <143256804+yadav-mohit-17@users.noreply.github.com>
2025-01-25 20:16:28 +0530

    update

---
+++ SpringBoot_Security_DBAuth/src/main/java/com/app/security/repo/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.app.security.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.security.entity.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}
